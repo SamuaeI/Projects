7c157941a6b97febfce76b30f5cdcd9294535897
RSamuaeI <retardsamuaeljlemon@gmail.com>
2021-06-15 12:25:50 -0400

    session time

--- Python/MultPrac.py
+++ Python/MultPrac.py
@@ -1,30 +1,36 @@
 import random
+import time
 import math
 
 ChosenMultiple = input("Chose a multiple: ")
+VeryStartTime = time.time()
 Correct, Incorrect = 0, 0
 
 while True:
     if not ChosenMultiple.isnumeric():
         ChosenMultiple = str(random.randrange(1, 12))
-
+    QuestionStartTime = time.time()
+    
     SecondMultiple = random.randrange(1, 12)
     QuestionSolution = int(ChosenMultiple) * SecondMultiple
     Answer = input(str(ChosenMultiple) + " x " + str(SecondMultiple) + " = ")
 
     if Answer.lower() == "end":
+        endTime = time.time()
         strCorrect = "You got "+  str(Correct) + " Correct"
         strIncorrect = "And you got "+  str(Incorrect) + " Incorrect"
         strScore = "Score: " + str(math.floor((Correct / (Correct + Incorrect)) * 100)) + "%"
-        maxStrLen = max([len(strCorrect), len(strIncorrect), len(strScore)])
+        strSessionTime = "Session time: " + str(round((endTime - VeryStartTime), 1)) + " seconds"
+        maxStrLen = max([len(strCorrect), len(strIncorrect), len(strScore), len(strSessionTime)])
 
         print("-"*maxStrLen)
-        print(strCorrect + "\n" + strIncorrect + "\n" + strScore)
+        print(strCorrect + "\n" + strIncorrect + "\n" + strScore + "\n" + strSessionTime)
         print("-"*maxStrLen)
         break
-        
+
     if Answer.isnumeric() and int(Answer) == QuestionSolution:
-        print("Correct")
+        stopTime = time.time()
+        print("Correct, it took you, " + str(round((stopTime - QuestionStartTime), 1)) + " seconds")
         Correct += 1
     else:
         print("Incorrect")
