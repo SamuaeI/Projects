0230aa011bf665c50d5f4900ceedf872c93c20fc
SamuaeI <retardsamuaeljlemon@gmail.com>
2021-06-15 01:23:46 -0400

    Create MultPrac.py

---
+++ MultPrac.py
@@ -0,0 +1,16 @@
+import random
+
+ChosenMultiple = input("Chose a multiple: ")
+
+while True:
+    if not ChosenMultiple.isnumeric():
+        ChosenMultiple = str(random.randrange(0, 12))
+
+    SecondMultiple = random.randrange(0, 12)
+    QuestionSolution = int(ChosenMultiple) * SecondMultiple
+    Answer = input(str(ChosenMultiple) + " x " + str(SecondMultiple) + " = ")
+
+    if int(Answer) == QuestionSolution:
+        print("Correct")
+    else:
+        print("Incorrect")
