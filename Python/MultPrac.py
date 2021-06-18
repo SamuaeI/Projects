35b54abd29d6d49eff1179982f049929d39c618a
RSamuaeI <retardsamuaeljlemon@gmail.com>
2021-06-18 18:28:41 -0400

    life a little easier
    doesn't break when, "done" is typed as an answer
    always continues unless, "stop" is typed as an answer

--- Python/MultPrac.py
+++ Python/MultPrac.py
@@ -19,17 +19,28 @@ while True:
     QuestionSolution = int(ChosenMultiple) * SecondMultiple
     Answer = input(str(ChosenMultiple) + " x " + str(SecondMultiple) + " = ")
 
-    if Answer.lower() == "end":
+    if Answer.lower() == "done":
         endTime = time.time()
         strCorrect = "You got "+  str(Correct) + " Correct"
         strIncorrect = "And you got "+  str(Incorrect) + " Incorrect"
-        strScore = "Score: " + str(math.floor((Correct / (Correct + Incorrect)) * 100)) + "%"
+
+        if Correct != 0 and Incorrect != 0:
+            strScore = "Score: " + str(math.floor((Correct / (Correct + Incorrect)) * 100)) + "%"
+        else:
+            strScore = "Score: 0%"
+
         strSessionTime = "Session time: " + str(round((endTime - VeryStartTime), 1)) + " seconds"
         maxStrLen = max([len(strCorrect), len(strIncorrect), len(strScore), len(strSessionTime)])
 
         print("-"*maxStrLen)
         print(strCorrect + "\n" + strIncorrect + "\n" + strScore + "\n" + strSessionTime)
         print("-"*maxStrLen)
+
+        ChosenMultiple = input("Chose a multiple: ")
+        VeryStartTime = time.time()
+        Correct, Incorrect, LastMultiple = 0, 0, 0
+        continue
+    elif Answer.lower() == "stop":
         break
 
     if Answer.isnumeric() and int(Answer) == QuestionSolution:
